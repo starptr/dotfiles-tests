2fed61eefd13854fd6b77eb08e065fac9d56d205
Yuto Nishida <starptr@users.noreply.github.com>
2021-06-30 17:35:25 -0700

    Working deploy command

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,69 @@
-use anyhow::Result;
-use clap::{Arg, App};
+use anyhow::{Result, anyhow};
+use clap::{App, Arg, ArgMatches};
 use cmd_lib::*;
 use pretty_env_logger;
+use std::env;
+use dirs;
 
 #[rustfmt::skip::macros(run_cmd)]
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
+    use_builtin_cmd!(info, warn, error, die, cat);
 
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .subcommand(App::new("deploy")
-                    .about(format!("Mounts a build of {0} via docker-compose, and runs tests. Requires running {0} as a submodule of dotfiles.", env!("CARGO_PKG_VERSION")).as_str())
-                    .arg(Arg::with_name("dev")
-                         .short("d")
-                         .long("dev")
-                         .help("Use dev build."))
-                    .arg(Arg::with_name("release")
-                         .short("r")
-                         .long("release")
-                         .help("Use release build.")
-                         .conflicts_with("dev")))
+                    .about(format!("Mounts a build of {0} via docker-compose, and runs tests. Requires running {0} as a submodule of dotfiles.", env!("CARGO_PKG_NAME")).as_str()))
         .subcommand(App::new("run")
                     .about(format!("Runs tests. Should be used where dotfiles are installed.").as_str()))
         .get_matches();
 
-    use_builtin_cmd!(info, warn, error, die, cat);
+    match matches.subcommand() {
+        ("deploy", Some(deploy_matches)) => {
+            deploy(deploy_matches)
+        },
+        ("run", Some(run_matches)) => {
+            run(run_matches)
+        },
+        _ => {
+            Err(anyhow!("No command specified"))
+        },
+    }.map_err(|e| {
+        if run_cmd! (
+            docker-compose down 2>&1;
+        ).is_err() {
+            cmd_error!("Failed to stop containers.");
+        };
+        e
+    })
+}
 
-    cmd_info!("hello");
+fn deploy(deploy_matches: &ArgMatches) -> Result<()> {
+    let binary_path = env::current_exe();
+    let binary_path = binary_path.unwrap();
+    let binary_path = binary_path.to_str().unwrap();
+
+    let home_dir = dirs::home_dir().unwrap();
+    let home_dir = home_dir.to_str().unwrap();
+
+    env::set_var("POLKA_DOTS_BIN", binary_path);
+
     run_cmd! (
-        info "yah";
-        echo "echo test";
-        which yadm;
+        cd $home_dir;
+        docker-compose build 2>&1;
+        docker-compose up -d 2>&1;
     )?;
+    let container_id = run_fun!(docker-compose ps -q)?;
+    run_cmd! (
+        docker exec -it $container_id bash -c "expr 1 + 1";
+        docker-compose down 2>&1;
+    )?;
+    Ok(())
+}
+
+fn run(run_matches: &ArgMatches) -> Result<()> {
     Ok(())
 }
